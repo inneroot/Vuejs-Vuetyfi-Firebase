8b55bd075042036a888c4afec3d63ab50af4b966
Gleb Fedotov <innercoding@gmail.com>
2018-12-06 12:30:11 +0300

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+  <v-toolbar app></v-toolbar>
+  <v-content>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+  </v-content>
+  <v-footer app></v-footer>
+</v-app>
+</template>
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    name: 'App'
+  }
+</script>
